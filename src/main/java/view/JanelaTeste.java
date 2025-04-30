4d432f385321cd2b9e56755ff3baca41e05fe8cd
Francisco Pedrosa <francsp1@hotmail.com>
2025-04-30 13:12:49 +0100

    Janela Teste

---
+++ src/main/java/view/JanelaTeste.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class JanelaTeste extends JFrame {
+
+  public static void main(String[] args) {
+    JanelaTeste janela = new JanelaTeste();
+
+
+  }
+}
