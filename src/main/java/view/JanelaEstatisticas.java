2d4e13b7d907cfa13b4ad1963d763b07f19f2dfe
loupatfer <loupatfer@gmail.com>
2025-05-06 19:09:03 +0100

    Adicionada opção de escolher o periodo de tempo bem como recomendações para filmes baseado por idade

--- src/main/java/view/JanelaEstatisticas.java
+++ src/main/java/view/JanelaEstatisticas.java
@@ -8,6 +8,8 @@ public class JanelaEstatisticas {
     private JPanel mainPanel;
     private JTable table1;
     private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JList list1;
 
     public JanelaEstatisticas() {
         String[] columnNames = {"Posição no TOP", "Filme", "Realizador"};
