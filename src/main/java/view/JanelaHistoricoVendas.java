e0a2d52c2059cc0881d9cc67665b662f74b2000b
Tomás Santos <2230717@my.ipleiria.pt>
2025-05-06 22:39:42 +0100

    Janela para historico de vendas.

---
+++ src/main/java/view/JanelaHistoricoVendas.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class JanelaHistoricoVendas {
+    private JButton ordenarPorDataButton;
+    private JButton ordenarPorPreçoButton;
+    private JTable table1;
+}
