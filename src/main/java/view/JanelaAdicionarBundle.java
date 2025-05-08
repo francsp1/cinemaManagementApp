f925a6d41e51c235e8e4864fcc143c87a4c4f60d
Tomás Santos <2230717@my.ipleiria.pt>
2025-05-08 22:15:09 +0100

    janela de criação de bundles

---
+++ src/main/java/view/JanelaAdicionarBundle.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class JanelaAdicionarBundle {
+    private JList list1;
+    private JButton criarBundleButton;
+    private JButton cancelarOperaçãoButton;
+    private JTextField textField1;
+    private JTable table1;
+    private JButton adicionarButton;
+    private JButton removerButton;
+    private JTextField textField2;
+}
