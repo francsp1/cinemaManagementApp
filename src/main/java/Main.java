ffb3b94787977ededa73085d12aeb9b70e73a1d7
Francisco Pedrosa <francsp1@hotmail.com>
2025-05-14 14:56:33 +0100

    Renames Login screen's fields

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import view.JanelaLogin;
 import view.JanelaPrincipalGestor;
 
 public class Main {
     public static void main(String[] args) {
-        JanelaPrincipalGestor janelaPrincipal = new JanelaPrincipalGestor(null);
+        //JanelaPrincipalGestor janelaPrincipal = new JanelaPrincipalGestor(null);
+        JanelaLogin janelaLogin = new JanelaLogin();
+
     }
 }
--- src/main/java/view/JanelaLogin.java
+++ src/main/java/view/JanelaLogin.java
@@ -2,8 +2,35 @@ package view;
 
 import javax.swing.*;
 
-public class JanelaLogin {
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JButton loginButton;
+public class JanelaLogin extends JFrame {
+    private JPasswordField txtPassword;
+    private JButton btnLogin;
+    private JPanel pnlLogin;
+    private JLabel lblLogin;
+    private JLabel lblFuncionario;
+    private JPanel pnl1;
+    private JPanel pnl2;
+    private JPanel pnl3;
+    private JTextField lblUsername;
+    private JPanel pnl4;
+    private JLabel lblPassword;
+
+    public static void main(String[] args) {
+        JanelaLogin janela = new JanelaLogin();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public JanelaLogin() {
+        super("Lista de Salas");
+        setContentPane(pnlSalas);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+        btnSair.addActionListener(this::btnSairActionPerformed);
+
+        preencherListaSalas();
+
+        setVisible(true);
+    }
 }
