26c175fe6719d58060accbc6d5b80c0116ac5456
Rodrigo Correia <2231856@my.ipleria.pt>
2025-06-21 16:41:03 +0100

    passes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,7 @@ import view.JanelaLogin;
 public class Main {
     public static void main(String[] args) {
         JanelaLogin janelaLogin = new JanelaLogin();
+        //user:gestor1 password:1234
+        //user:funcionario1 password:1234
     }
 }
